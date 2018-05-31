794867f157bc5f3cac852e03361d2dfc2d6f4638
Schlosser Matthias <schlosserm@ict-campus.net>
2018-05-31 13:12:23 +0200

    Sensor 1.0

--- app/src/main/java/net/ictcampus/voektm/rapp/MainActivity.java
+++ app/src/main/java/net/ictcampus/voektm/rapp/MainActivity.java
@@ -1,44 +1,51 @@
 package net.ictcampus.voektm.rapp;
 
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.app.Activity;
+        import android.app.Service;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.graphics.Color;
+        import android.hardware.Sensor;
+        import android.hardware.SensorEvent;
+        import android.hardware.SensorEventListener;
+        import android.hardware.SensorManager;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.TypedValue;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
-import org.w3c.dom.Text;
+        import org.w3c.dom.Text;
 
-import java.util.List;
-import java.util.ServiceConfigurationError;
+        import java.util.List;
+        import java.util.ServiceConfigurationError;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
     TextView textView;
     SensorManager sensorManager;
     Sensor sensor;
-    private int[] themes = {R.style.Theme_RApp_Light_NoActionBar, R.style.RApp};
+    private int[] themes = {R.style.RApp_Light, R.style.RApp};
     private static int listPos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Activity mActivity = this;
         super.onCreate(savedInstanceState);
         setTheme(themes[listPos]);
         setContentView(R.layout.activity_main);
         View v = findViewById(R.id.logoView);
         textView = findViewById(R.id.textView);
-        v.setBackgroundColor(getResources().getColor(R.color.white));
 
+        TypedValue typedValue = new TypedValue();
+        mActivity.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int primaryColor = typedValue.data;
+        v.setBackgroundColor(primaryColor);
 
         sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
     }
 
     @Override
@@ -63,13 +70,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     listPos = 1;
                     Intent intent = getIntent();
                     finish();
+                    overridePendingTransition(0, 0);
                     startActivity(intent);
                 }
-            }else {
+            }else if(event.values[0] > 120){
                 if (listPos != 0) {
                     listPos = 0;
                     Intent intent = getIntent();
                     finish();
+                    overridePendingTransition(0, 0);
                     startActivity(intent);
                 }
             }
