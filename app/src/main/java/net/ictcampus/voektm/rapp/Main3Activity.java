4c824711d8a25d56160ec3d3e5de4f721406e739
Mischa <mischa.voekt@ict-campus.net>
2018-06-07 14:45:37 +0200

    asdas

---
+++ app/src/main/java/net/ictcampus/voektm/rapp/Main3Activity.java
@@ -0,0 +1,47 @@
+package net.ictcampus.voektm.rapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Main3Activity extends LightSensitivActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.play:
+                    mTextMessage.setText("Abos");
+                    return true;
+                case R.id.news:
+                    mTextMessage.setText("News");
+                    return true;
+                case R.id.favorites:
+                    mTextMessage.setText("Favoriten");
+                    return true;
+                case R.id.search:
+                    mTextMessage.setText("Suche");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
