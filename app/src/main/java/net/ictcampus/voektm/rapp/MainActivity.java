c829c34bccd8cf9307da775365d2a02e60261c87
Schlosser Matthias <schlosserm@ict-campus.net>
2018-06-14 14:25:57 +0200

    Merge

--- app/src/main/java/net/ictcampus/voektm/rapp/MainActivity.java
+++ app/src/main/java/net/ictcampus/voektm/rapp/MainActivity.java
@@ -48,7 +48,9 @@ public class MainActivity extends LightSensitivActivity{
     SQLiteDatabase db;
     SQLiteOpenHelper manager;
     ArrayList<ImageItem> data;
+    //Namen der Rapper in der Reihenfolge, wie die Bilder
     private String[] namesofrappers = {"Azet","Capital Bra","Farid Bang","Dardan","Kollegah","Kontra K","Miami Yacine","Raf Camora","187","UFO361","Zuna","Luciano","18 Karat","Bushido","Capo","Eno","Class X","Nimo"};
+    //IDs der Rapper in der Reihenfolge wie die Namen / Bilder
     private String[] idsofrappers = {"UCDLPHv0SrvyUzct8UYQ9R_g","UCGU9EqK5V5m141sNPCOfRBg","UCbDNCzgdLlvYY9dk5M8063A", "UCqv9TYpXUAo-Qy_tOPPSUYg","UCzmO7GegLke-jb5uZSQ9_HA", "UCIgCb1dYprH140Ds0mgeAUA", "UCDLPHv0SrvyUzct8UYQ9R_g", "UChqcJ_MhP9a4bXy1jQ0QPzQ", "UCGh8tmH9x9njaI2mXfh2fyg", "UCXK2490SNd8EOm84Es6USjw", "UCDLPHv0SrvyUzct8UYQ9R_g", "UCVWm9bTQLmMwHI0To5zQFGA", "UCbDNCzgdLlvYY9dk5M8063A", "UCMHWpfahk3Kt8Us28Jg51nw","UCpIHbzhK3-sHxuGsahKacNA","UCqbKhBvxM3qWzC8huPiifYg","","UCvnCXuh_zhm75EJ89qJ95Kw"};
 
     @Override
@@ -56,23 +58,28 @@ public class MainActivity extends LightSensitivActivity{
         Activity mActivity = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnFertig = (Button) findViewById(R.id.btnFertig);
+        //Datenbank
         manager = RAppDB.getInstance(this);
         db = manager.getWritableDatabase();
 
+        //Textview Titel
         TextView auswahl = findViewById(R.id.txtAuswahl);
-        auswahl.setText("Wählen sie Ihre  Lieblingsapper aus");
-        gridView = (GridView) findViewById(R.id.gridView);
-        data = getData();
-        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, data);
-        gridView.setAdapter(gridAdapter);
+        auswahl.setText("Wählen Sie Ihre  Lieblingsapper aus");
 
+        //Button Fertig
+        btnFertig = (Button) findViewById(R.id.btnFertig);
         btnFertig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fertig_click();
             }
         });
+
+        //Gridview
+        gridView = (GridView) findViewById(R.id.gridView);
+        data = getData();
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, data);
+        gridView.setAdapter(gridAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 
